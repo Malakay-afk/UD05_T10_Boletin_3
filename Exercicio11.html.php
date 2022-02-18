8f6d08bd48473e7036129c63c7e39e29220af82f
Miguel García <mikelrapsolo@gmail.com>
2022-02-18 09:51:15 +0100

    exercicio minutos

--- Exercicio11.html.php
+++ Exercicio11.html.php
@@ -4,11 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>boletín 3 - exercicio 10*</title>
+    <title>boletín 3 - exercicio 11</title>
 </head>
-<body>
+<body>   
+<form action="<?php echo $_REQUEST['PHP']?>" method="post">
+        <p>Escribe la hora: <input type="text" name="n"></p>
+        <p>Escribe la hora: <input type="text" name="m"></p>
+        <input type="submit" value="enviar">
+
+</form>
     <?php
-       
+          $numero = ($_REQUEST['n']* 60 * 60)+($_REQUEST('m') * 60);
+          $segundosDia = (24 * 60 * 60);
+      
+        $resultado = ($numero - $segundosDia);
+        
+    echo 'quedan $resultado segundos para llegar a medianoche'
     ?>
 </body>
 </html>
